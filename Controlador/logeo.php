899820f5646c2bdb30b2a30456120c1ea6bec773
ganeko.94 <ganeko.94@gmail.com>
2017-01-25 13:26:09 +0100

    Actualisasion

--- Controlador/logeo.php
+++ Controlador/logeo.php
@@ -5,3 +5,31 @@
  * Date: 25/1/17
  * Time: 13:05
  */
+
+require_once '../Modelo/Usuario.php';
+require_once '../Modelo/GenericoBD.php';
+
+$conexion = conectar();
+
+$nomusuario = $_POST["usuario"];
+$contrasena = $_POST["contrasena"];
+
+$usuario = new Usuario();
+
+$consulta = "SELECT * FROM Usuario WHERE NomUsuario ='".$nomusuario."' AND Contrasena = '\".$contrasena.\"'";
+
+$resultado = mysqli_query($conexion, $consulta);
+
+$fila = mysqli_fetch_object($resultado);
+
+if($resultado->num_rows !=0){
+    session_start();
+
+    $_SESSION["usuario"] = $fila->NomUsuario;
+
+    header('Location: ../Vista/Principal.php');
+}else{
+    echo 'false';
+}
+
+desconectar($conexion);
---
+++ Vista/Principal.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 2gdwes04
+ * Date: 25/1/17
+ * Time: 13:22
+ */
+
+echo "Esta es la página principal";
+
+echo '<html><a href="../index.php">Pincha aquí para volver al inicio</a></html>';
+?>
